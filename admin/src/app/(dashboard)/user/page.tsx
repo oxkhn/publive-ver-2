53cfb0e6afddfd817356ce4d501558fc74035ff8
icii.dev <150992135+oxkhn@users.noreply.github.com>
2024-10-28 16:16:12 +0700

    2

--- admin/src/app/(dashboard)/user/page.tsx
+++ admin/src/app/(dashboard)/user/page.tsx
@@ -1,16 +1,9 @@
 'use server'
 
-import { db as userData } from '@/data/userList'
-import UserList from '@/views/app/user/list'
+import UserList from "@/views/app/user/list"
 
-export const getUserData = async () => {
-    return userData
-}
-
-const UserPage =async () => {
-    const data = await getUserData()
-
-    return <UserList userData={data} />
+const UserPage = async () => {
+    return <UserList />
 }
 
 export default UserPage
--- admin/src/views/app/user/list/index.tsx
+++ admin/src/views/app/user/list/index.tsx
@@ -8,14 +8,22 @@ import UserListTable from './UserListTable'
 import UserListCards from './UserListCards'
 import { UsersType } from '@/types/userTypes'
 
-const UserList = ({ userData }: { userData?: UsersType[] }) => {
+import { db as userData } from '@/data/userList'
+
+export const getUserData = async () => {
+    return userData
+}
+
+const UserList = async () => {
+    const data = await getUserData()
+
     return (
         <Grid container spacing={6}>
             <Grid item xs={12}>
                 <UserListCards />
             </Grid>
             <Grid item xs={12}>
-                <UserListTable tableData={userData} />
+                <UserListTable tableData={data} />
             </Grid>
         </Grid>
     )

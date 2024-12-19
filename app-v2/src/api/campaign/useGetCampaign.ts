53cfb960804bf4f3e4ba072a06e2f5166e6eada4
icii.dev <150992135+oxkhn@users.noreply.github.com>
2024-12-19 19:34:34 +0700

    _update: ui

--- app-v2/src/api/campaign/useGetCampaign.ts
+++ app-v2/src/api/campaign/useGetCampaign.ts
@@ -1,11 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { axiosWithoutAccessToken } from "../../../axiosConfig/axiosConfig";
 
 export const useGetCampaign = () => {
   return useMutation({
     mutationKey: ["GET_CAMPAIGN"],
     mutationFn: async (campaignId: string) => {
-      const res = await axios.get("/api/campaign/" + campaignId);
+      const res = await axiosWithoutAccessToken.get("/api/campaign/" + campaignId);
       return res.data.data;
     },
   });
